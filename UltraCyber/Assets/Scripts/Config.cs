3097b44b2e83d904b3a647790bf7b20ae1c30869
SeruK <strandsebastian2000@gmail.com>
2014-05-03 01:27:58 +0200

    Removed player grav

--- UltraCyber/Assets/Scripts/Config.cs
+++ UltraCyber/Assets/Scripts/Config.cs
@@ -5,9 +5,7 @@ public class Config : MonoBehaviour
 {
 	public float playerMovementForce;
 	public float playerJumpForce;
-
-	public Vector2 playerGravity = new Vector2(0.0f, -1.0f);
-
+	
 	public float maxSpeed = 10.0f;
 
 	public int shots = 6;
