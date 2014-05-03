48deb279046cb6ac9e4b031b0d351d8ff2ff8c79
Agaton Andre <handycapz@gmail.com>
2014-05-03 14:20:44 +0200

    lots of shit

---
+++ UltraCyber/Assets/Scripts/CamMong.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMong : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//if(GameInput.GetXboxButton(0, GameInput.Xbox360Button.A) || Input.GetKey(KeyCode.Space) 
+		//player.input.shoot = GameInput.GetXboxButtonDown(0, GameInput.Xbox360Button.B) || Input.GetKeyDown(KeyCode.X);
+
+		Camera.main.backgroundColor = Helper.GetRandomColor();
+	
+	}
+}
---
+++ UltraCyber/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (uint i = 0; i < 2; ++i)
+		{
+			if (Input.GetKeyDown(KeyCode.Space) || GameInput.GetXboxButton(i, GameInput.Xbox360Button.A) || GameInput.GetXboxButtonDown(i, GameInput.Xbox360Button.B)) {
+				Application.LoadLevel(1);
+			}
+
+		}
+	
+	}
+}
