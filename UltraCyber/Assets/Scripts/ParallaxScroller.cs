eb72069250633d6454a399fc4573f10153e8d3b6
Agaton Andre <handycapz@gmail.com>
2014-05-03 12:45:07 +0200

    backgrounds n stufz

---
+++ UltraCyber/Assets/Scripts/ParallaxScroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroller : MonoBehaviour {
+
+
+	[SerializeField]
+	private float camMovementMultiplier;
+
+	public Vector2 constantSpeed = Vector2.zero;
+
+	public Camera cam;
+
+	private Vector2 oldCamPos;
+	void Start () {
+		oldCamPos = cam.transform.position;
+	}
+	
+	//save up waypoints
+	void Update () {
+		Vector2 toGo = (Vector2)cam.transform.position - oldCamPos;
+
+		transform.position += (Vector3)(toGo *camMovementMultiplier);
+		oldCamPos = cam.transform.position;
+	}
+}
